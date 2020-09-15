84aca27774fc512bf42988f647aae859ee65cb1f
Valentin <val.fediuk@gmail.com>
2020-09-15 15:10:32 +0200

    feat: build

---
+++ build/precache-manifest.0d579b4cc198f8edb75a25245670eb35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484d1822c726d3dbc6e3d7c6d34f4cd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f36d83992db3ec67d70b",
+    "url": "/static/css/main.7c1d7bcc.chunk.css"
+  },
+  {
+    "revision": "2ddef6eef44c3106aeef",
+    "url": "/static/js/2.2c13ee44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2c13ee44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36d83992db3ec67d70b",
+    "url": "/static/js/main.22f26021.chunk.js"
+  },
+  {
+    "revision": "d049e954b1c74f29178c",
+    "url": "/static/js/runtime-main.9c7c50c1.js"
+  }
+]);
